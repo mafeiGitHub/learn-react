a5218b0fb0b42075e949c86c56c3c738b5589dd6
mafeiGitHub <mwf1998@qq.com>
2024-02-28 21:51:26 +0800

    init

---
+++ src/01_组件的定义方式/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+        message:"你好啊"
+    }
+  }
+  render() {
+    return(
+        <div>
+            <span>我是App组件</span>
+            <h3>你好小飞</h3>
+        </div>
+    )
+  }
+}
